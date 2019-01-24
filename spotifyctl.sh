1fa0734e549cfa93e15f8b402f3cfe7e29a7103a
Christian Kaubisch <christiankaubisch@gmail.com>
2019-01-24 13:00:06 +0100

    added script

---
+++ spotifyctl.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# spotifyctl.sh
+#
+# control spotify-player over CLI
+
+play() {
+	dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play
+}
+
+pause() {
+	dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause
+}
+
+playpause_toggle() {
+	dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
+}
+
+next() {
+	dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next
+}
+
+prev() {
+	dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous
+}
+
+case "$1" in
+	play)
+		play;;
+	pause)
+		pause;;
+	playpause_toggle)
+		playpause_toggle;;
+	next)
+		next;;
+	prev)
+		prev;;
+	
+	*)
+	echo "Usage: $0 (play|pause|playpause_toggle|next|prev)";;
+esac
+
